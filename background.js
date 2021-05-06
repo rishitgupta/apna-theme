e9a4fd3dd7b8e8826690de09dca846ffc64aaf8d
rishitgupta <rishit@theguptafamily.com>
2021-05-07 01:42:48 +0530

    Added popup.html, button,css and placeholder icons

--- background.js
+++ background.js
@@ -2,5 +2,5 @@ let color = "#00ff00";
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
-    console.log('Default background color changed to green.', `color: ${color}`)
+    console.log('Default background color changed to green.', `color: ${color}`);
 });
