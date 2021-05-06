3f7b21272edf4bb8f4bbdfb5d517ff9e8b9027ff
rishitgupta <rishit@theguptafamily.com>
2021-05-06 23:47:24 +0530

    Added storage permission

---
+++ background.js
@@ -0,0 +1,6 @@
+let color = "#00ff00";
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({ color });
+    console.log('Default background color changed to green.', `color: ${color}`)
+});
