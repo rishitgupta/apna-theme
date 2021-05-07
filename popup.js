759bf58804ed4f4ea243e28ed6a59d975109ab29
rishitgupta <rishit@theguptafamily.com>
2021-05-07 12:55:00 +0530

    Added svg

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-let themeBuilder = document.getElementById('themeBuilder');
+let themeBuilder = document.getElementById('theme-builder');
 
 chrome.storage.sync.get("color", ({ color }) => {
     themeBuilder.style.backgroundColor = color;
